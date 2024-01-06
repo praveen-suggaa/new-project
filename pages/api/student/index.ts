972e3813750aa93236d2e28fc7a58677e0877248
praveen-suggaa <praveen@suggaa.in>
2024-01-06 13:02:05 +0530

    updating the connection

--- pages/api/student/index.ts
+++ pages/api/student/index.ts
@@ -1,13 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import {sql} from '@vercel/postgres'
-
+import { createPool } from '@vercel/postgres';
+const pool = createPool({
+    connectionString: process.env.POSTGRES_URL
+});
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
     if(req.headers["secret"] === '123'){
      const {id,name} = req.body
-     const {rows} = await sql`select * from  students  where id = ${id}`;
+     const {rows} = await pool.sql`select * from  students  where id = ${id}`;
   res.status(200).json({rows})
 }
 else {
