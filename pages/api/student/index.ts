8f4c6540810ebef33ebe79435856355f35827494
praveen-suggaa <praveen@suggaa.in>
2024-01-12 14:28:50 +0530

    modified index such that it will show data on route

--- pages/api/student/index.ts
+++ pages/api/student/index.ts
@@ -7,12 +7,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    if(req.headers["secret"] === '123'){
+    // if(req.headers["secret"] === '123'){
      const {id,name} = req.body
-     const {rows} = await pool.sql`select * from  students  where id = ${id}`;
+     const {rows} = await pool.sql`select * from students`;
   res.status(200).json({rows})
-}
-else {
+// }
+// else {
     res.status(500).json({message:"internal server error"})
-}
+// }
 }
