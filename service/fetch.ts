5d63e18bebe97c60e6cb6b3eb2e407d462923f39
praveen-suggaa <praveen@suggaa.in>
2024-01-13 11:43:50 +0530

    WIP

--- service/fetch.ts
+++ service/fetch.ts
@@ -1,25 +1,28 @@
-// pages/index.js
 const HOST = process.env.NEXT_PUBLIC_HOST;
 
-export async function fetchStudent() {
-  // Fetch data from your API
+async function fetchStudents() {
   try {
     const apiUrl = `${HOST}/api/student`;
     const response = await fetch(apiUrl, {
       headers: {
-        "Content-Type": "application/json",
-        // secret: secret,
+        'Content-Type': 'application/json',
       },
     });
 
     if (!response.ok) {
-      throw new Error("Failed to fetch data from API");
+      throw new Error('Failed to fetch data from API');
     }
 
     const data = await response.json();
+
+    if (!data || !data.rows) {
+      throw new Error('Invalid data format');
+    }
+
     return data;
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error('Error fetching data:', error);
   }
 }
-export default fetchStudent;
+
+export default fetchStudents;
